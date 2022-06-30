8800c138f441d770af990c728544d75dc72eee13
Sümeyye <52049640+smyyyaydn@users.noreply.github.com>
2022-06-30 04:58:05 +0300

    Create untitled8.py

---
+++ untitled8.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 25 22:34:06 2022
+
+@author: smyyy
+"""
+
+# x=list(range(1,5))
+# y=[i for i in x]
+# z=[i**2 for i in x]
+# print(x)
+    
+
+# z=[[i,i**2] for i in x]
+# print(z)
+
+
+# x=list(range(1,5))
+# z=[{str(i):i} for i in x]
+# print(z)
+
+# x={"a",1,2,3,4,"b","c",5,6,"d"}
+# y=[i for i in x if str(i).isdigit()]
+# print(y)
+
+# x=[i**2 for i in[1,2,3]]
+# print(x)
+
+
+# def kareal(liste):
+#     for i in liste:
+#         yield i*i
+#         x=[1,2,3,4,5]
+#         y=kareal(x)
+#         print(y)
+
+def çarp(a,b):
+    return a*b
+çarp1=  lambda a,b:a*b
+print(çarp(5,6))
+  
+    
